3f7d2fda286d72f7c637762ca4eaee7574b1c8a8
Axel Obscura <axosar@gmail.com>
2019-05-01 12:54:16 -0500

    Bangs
    Adding bangs to strings

--- hello.js
+++ hello.js
@@ -9,10 +9,12 @@ class Hello extends React.Component {
     render() {
         const num = getNum();
         const props = this.props;
+        const bangs = "!".repeat(this.props.bangs);
         console.log(this.props);
         return (
             <div>
-                <p>Hi {props.to} from {props.from}</p>
+                <p>Hi {props.to} from {props.from}{bangs}</p>
+                <img src={this.props.img} />
                 <h1>My Ncurrent mood is: {getMood()}</h1>
                 <h2>Your number is: {num}</h2>
                 <h3>{num === 7 ? "Lucky!!!" : "Unlucky"}</h3>
--- index.js
+++ index.js
@@ -2,9 +2,33 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <Hello to="bandit" from="kuka" />
-                <Hello to="Cher" from="Wakanda" />
-                <Hello to="Patito" from="Canarito" />
+                <Hello
+                    to="bandit"
+                    from="kuka"
+                    num={3}
+                    data={[1, 2, 3, 4, 5]}
+                    isFunny={true}
+                    bangs={4}
+                    img={"https://images.unsplash.com/photo-1556442717-2eb4645c8875?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"}
+                />
+                <Hello
+                    to="Cher"
+                    from="Wakanda"
+                    num={15}
+                    data={[1, 2, 3, 4, 5]}
+                    isFunny={true}
+                    bangs={4}
+                    img={"https://images.unsplash.com/photo-1556442717-2eb4645c8875?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"}
+                />
+                <Hello
+                    to="Patito"
+                    from="Canarito"
+                    num={25}
+                    data={[1, 2, 3, 4, 5]}
+                    isFunny={false}
+                    bangs={4}
+                    img={"https://images.unsplash.com/photo-1556442717-2eb4645c8875?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"}
+                />
             </div>
         )
     }
